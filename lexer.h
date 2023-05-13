3ef1ef9fa16b0bed878cb99da8a0e6e169e735c8
cgeo <93139137+Geooooooooooo@users.noreply.github.com>
2023-05-13 12:23:06 +0000

    Update lexer.h

--- lexer.h
+++ lexer.h
@@ -1,6 +1,5 @@
 #include "dependencies.h"
 
 void daf_get_next_token(Source* source, DafTokenType* t_token_type, char* token_value);
-_Bool daf_is_number(char c);
-_Bool daf_is_alpha(char c);
-_Bool daf_is_operator(char c);
+DafTokens daf_parse_tokens(Source* source);
+void daf_free_tokens(DafTokens* daf_tokens);
