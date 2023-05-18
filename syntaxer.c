a9787ee3e8f8a17c06f2382c62372bc26e071633
cgeo <93139137+Geooooooooooo@users.noreply.github.com>
2023-05-18 09:38:12 +0000

    Added () to +- syntaxer.c

--- syntaxer.c
+++ syntaxer.c
@@ -5,8 +5,8 @@
 #define cur yel_tokens->value[i]
 
 size_t i = 0;
-int rec_mul_div = 0;
-_Bool last_mul_div = 0;
+int recursive_descent = 0;
+int brakets = 0;
 
 void yel_parse_expression(YelTokens* yel_tokens) {
     while (i < yel_tokens->length) {
@@ -14,70 +14,104 @@ void yel_parse_expression(YelTokens* yel_tokens) {
             return;
         }
 
-        if (next[0] == '*' || next[0] == '/') {
-            puts(cur);
-            puts(yel_tokens->value[i+2]);
-            puts(next);
+        if (cur[0] == '(') {
+            ++i;
+            ++brakets;
+            yel_parse_expression(yel_tokens);
+        }
+        else if (cur[0] == ')') {
+            ++i;
+            --brakets;
+            return;
+        }
+        else if (next[0] == '*' || next[0] == '/') {
+            printf("%s %s %s ", cur, yel_tokens->value[i+2], next);
 
             i += 2;
         }
         else if(cur[0] == '*' || cur[0] == '/') {
-            puts(next);
-            puts(cur);
-
+            printf("%s %s ", next, cur);
             ++i;
         }
         else if (next[0] == '+' || next[0] == '-') {
-            if (rec_mul_div) {
-                --rec_mul_div;
+            if (recursive_descent) {
+                --recursive_descent;
                 return;
             }
 
-            if (yel_tokens->value[i+3][0] == ';' || yel_tokens->value[i+3][0] == '+' || yel_tokens->value[i+3][0] == '-') {
-                puts(cur);
-                puts(yel_tokens->value[i+2]);
-                puts(next);
+            size_t gTmp_i = i+3;
+
+            if (yel_tokens->value[i+2][0] == '(') {
+                printf("%s ", cur);
+                size_t tmp_i = i + 1;
+
+                i += 3;
+                ++brakets;
+                yel_parse_expression(yel_tokens);
+
+                if (cur[0] == '*' || cur[0] == '/') {
+                    ++recursive_descent;
+                    yel_parse_expression(yel_tokens);
+                }
+
+                printf("%s ", yel_tokens->value[tmp_i]);
+                continue;
+            }
+            else if (yel_tokens->value[gTmp_i][0] == ';' || yel_tokens->value[gTmp_i][0] == '+' || yel_tokens->value[gTmp_i][0] == '-'|| yel_tokens->value[gTmp_i][0] == ')') {
+                printf("%s %s %s ", cur, yel_tokens->value[i+2], next);
 
                 i += 2;
-                // i += 3;
             }
-            else if (yel_tokens->value[i+3][0] == '*' || yel_tokens->value[i+3][0] == '*') {
-                puts(cur);
-                size_t tmp_i = ++i;
-                ++i;
-                ++rec_mul_div;
+            else if (yel_tokens->value[gTmp_i][0] == '*' || yel_tokens->value[gTmp_i][0] == '/') {
+                printf("%s ", cur);
+
+                size_t tmp_i = i + 1;
+                i += 2;
+                ++recursive_descent;
 
                 yel_parse_expression(yel_tokens);
 
-                puts(yel_tokens->value[tmp_i]);
+                printf("%s ", yel_tokens->value[tmp_i]);
             }
         }
+        // cur = + or -
         else if(cur[0] == '+' || cur[0] == '-') {
-            if (rec_mul_div) {
+            if (recursive_descent) {
                 --i;
-                --rec_mul_div;
+                --recursive_descent;
                 return;
             }
-
-            if (yel_tokens->value[i+2][0] == ';' || yel_tokens->value[i+2][0] == '+' || yel_tokens->value[i+2][0] == '-') {
-                puts(next);
-                puts(cur);
-
-                ++i;
-            }
-            else if (yel_tokens->value[i+2][0] == '*' || yel_tokens->value[i+2][0] == '*') {
-                puts(prev);
+            // expr + ( expr )
+            if (next[0] == '(') {
                 size_t tmp_i = i;
-                ++i;   
-                ++rec_mul_div;
-
+                i += 2;
+                ++brakets;
                 yel_parse_expression(yel_tokens);
 
-                puts(yel_tokens->value[tmp_i]);
+                printf("%s ", yel_tokens->value[tmp_i]);
+                continue;
+            }
+            else if (yel_tokens->value[i+2][0] == ';' || yel_tokens->value[i+2][0] == '+' || yel_tokens->value[i+2][0] == '-'|| yel_tokens->value[i+2][0] == ')') {
+                printf("%s %s ", next, cur);
+                ++i;
+            }
+            else if (yel_tokens->value[i+2][0] == '*' || yel_tokens->value[i+2][0] == '/') {
+                size_t tmp_i = i;
+                ++i;
+                ++recursive_descent;
+                yel_parse_expression(yel_tokens);
+
+                printf("%s ", yel_tokens->value[tmp_i]);
             }
         }
-        else if(yel_tokens->type[i] == tok_name || yel_tokens->type[i] == tok_number) {
-            puts(cur);
+        else if (yel_tokens->type[i] == tok_number) {
+            printf("%s ", cur);
+        }
+        else if (yel_tokens->type[i] == tok_string) {
+            printf("string(%s) ", cur);
+        }
+        else if (yel_tokens->type[i] == tok_name) {
+            printf("%s ", cur);
         }
 
         ++i;
@@ -87,7 +121,7 @@ void yel_parse_expression(YelTokens* yel_tokens) {
 void yel_gen_parse_tree(YelTokens* yel_tokens) {
     while (i < yel_tokens->length) {
         if (__builtin_strcmp(yel_tokens->value[i], "=") == 0) {
-            printf("assing(%s expr( \n", yel_tokens->value[i - 1]);
+            printf("assing(%s expr( ", yel_tokens->value[i - 1]);
             
             ++i;
             yel_parse_expression(yel_tokens);
