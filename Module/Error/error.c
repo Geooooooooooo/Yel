2a0333c0bb856a36f2a1d2ee3926ec015ff0af82
cgeo <93139137+Geooooooooooo@users.noreply.github.com>
2023-06-20 15:56:25 +0000

    Add files via upload

--- Module/Error/error.c
+++ Module/Error/error.c
@@ -1,64 +1,47 @@
 #include "error.h"
 
 void yel_print_error(const char* err_name, const char* err_desc, Source* src, size_t line, size_t symbol) {
-    printf("File '%s', line %lu\n\n", src->file_name, line, symbol);
+    printf("File '%s', line %lu\n\n    ", src->file_name, line, symbol);
 
-    --symbol;
     size_t line_counter = 1;
-    size_t symbol_counter = 1;
 
     for (size_t i = 0; i < src->length; i++) {
         if (line_counter == line) {
-            size_t i_start = i;
-            size_t i_end = i;
+            size_t symbol_counter = 1;
+            size_t start = i;
 
-            while (src->source_text[i_end] == ' ') ++i_end;
+            while (src->source_text[start] == ' ') {
+                ++start;
+                ++symbol_counter;
+                ++i;
+            }
+
+            for (size_t l = start; l < src->length; l++) {
+                if (src->source_text[l] == '\n') break;
+                printf("%c", src->source_text[l]);
+                if (src->source_text[l] == ';') break;
+            }
+            
+            printf("\n    ");
             while (1) {
-                if ((src->source_text[i_end] == ';' && symbol_counter >= symbol) || src->source_text[i_end] == '\n' || src->source_text[i_end] == '\0') {
-                    ++i_end;
-                    break;
-                }
-                else if (src->source_text[i_end] == ';' || (src->source_text[i_end - 1] == '*' && src->source_text[i_end] == '/')) {
-                    i_start = i_end + 1;
-                }
+                if (src->source_text[i] == '\n') break;
 
-                ++i_end;
+                else if (symbol_counter == symbol) printf("^");
+                else printf(" ");
+
+                if (src->source_text[i] == ';') break;
+
+                ++i;
                 ++symbol_counter;
             }
 
-            printf("    ");
-            for (size_t l = i_start; l < src->length; l++) {
-                if (src->source_text[l] == '\n')
-                    break;
-
-                printf("%c", src->source_text[l]);
-
-                if (src->source_text[l] == ';')
-                    break;
-            }
-
-            i_end -= i;
-
-            printf("\n    ");
-            for (size_t l = i_start-i; l < i_end; l++) {
-                if (l == symbol) {
-                    printf("^");
-                }
-                else printf("~");
-            }
-            
-            puts("\n");
-
-            printf("%s: %s\n", err_name, err_desc);
-
-            return;
+            break;
         }
-
-        if (src->source_text[i] == '\n') {
+        else if (src->source_text[i] == '\n') {
             ++line_counter;
-            symbol_counter = 1;
         }
-
-        ++symbol_counter;
     }
+
+    printf("\n\n%s: %s\n", err_name, err_desc);
+
 }
