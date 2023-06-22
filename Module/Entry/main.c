568c9292a663cf6098052c39b33ee5b3a450bc57
cgeo <93139137+Geooooooooooo@users.noreply.github.com>
2023-06-22 16:09:11 +0000

    Add files via upload

--- Module/Entry/main.c
+++ Module/Entry/main.c
@@ -1,13 +1,20 @@
-#include "io.h"
-#include "lexer.h"
-#include "syntaxer.h"
-#include "dependencies.h"
+#include "../IO/io.h"
+#include "../Lexer/lexer.h"
+#include "../Parser/syntaxer.h"
+#include "../Parser/parser.h"
+#include "../Dependencies/dependencies.h"
 
-// TODO: unclosed block -> { ... and ...}
+// TODO: create checker to (-q -= (1 - 2) ** 3);
+//                             ^^
+//                             invalid syntax
+
+// IN FUTURE: Instead of each bin_op use '''bin_op 10'''
+
+// ERROR: print((add(1 - 2) - - 2));
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        printf("No input file\nUse: yel <filename>\n");
+        printf("No input file\nUse: 'yel <filename>'\n");
         return -1;
     }
 
@@ -21,9 +28,9 @@ int main(int argc, char* argv[]) {
         goto _yel_end;
     }
 
-    //print_tokens();
+    yel_gen_opcode(&token_array);
 
-    yel_gen_parse_tree(&token_array);
+    puts("load_const None\nret");
 
 _yel_end:
     yel_free_tokens(&token_array);
