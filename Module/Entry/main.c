b7ad56356bad541a4313acf348cb9ea384e8a53f
cgeo <93139137+Geooooooooooo@users.noreply.github.com>
2023-06-24 17:53:40 +0000

    Add files via upload

--- Module/Entry/main.c
+++ Module/Entry/main.c
@@ -4,13 +4,13 @@
 #include "../Parser/parser.h"
 #include "../Dependencies/dependencies.h"
 
+#include <stdlib.h>
+
 // TODO: create checker to (-q -= (1 - 2) ** 3);
 //                             ^^
 //                             invalid syntax
 
 // IN FUTURE: Instead of each bin_op use '''bin_op 10'''
-
-// ERROR: print((add(1 - 2) - - 2));
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -28,9 +28,12 @@ int main(int argc, char* argv[]) {
         goto _yel_end;
     }
 
-    yel_gen_opcode(&token_array);
+    OPCODES codes;
+    yel_gen_opcode(&token_array, &codes);
 
-    puts("load_const None\nret");
+    for (size_t i = 0; i < codes.len; i++) {
+        printf("%llu\n", codes.codes[i]);
+    }
 
 _yel_end:
     yel_free_tokens(&token_array);
